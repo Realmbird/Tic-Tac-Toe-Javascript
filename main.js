60569cc18bc011c5d9ecadecd0e10bd7c8d1970b
Realmbird <happychristopher777@gmail.com>
2024-01-08 11:29:42 -0800

    Fixed checkrow checkcol and checkdialgonal

--- main.js
+++ main.js
@@ -4,7 +4,33 @@ function gameBoard() {
     const rows = 3;
     const columns = 3;
     let board = [];
-
+    const setTile = (playerNum, row, col) => {
+        /*
+        Gonna be like player (1) or player (2)
+        Excepting Number
+        */
+        const player = playerNum
+        //added symbol not var so it is still inside of function scope
+        let symbol;
+    
+        switch(player){
+            case 1:
+                symbol = 'x';
+                break;
+            case 2:
+                symbol = 'o';
+                break;
+            default:
+                console.log("Symbol not working");
+                symbol = "";
+        }
+        /*
+        private variable symbol is set
+        Closure is used so function just needs coordinates (row,col)
+        */
+        board[row][col].addToken(symbol)
+        
+    }
     const generateBoard = () => {
         /*
         0,1,2
@@ -30,11 +56,12 @@ function gameBoard() {
         */
         for(let i = 0; i < rows; i++){
             // checks values for row equal and not equal to empty acts as unless if true this statement will not run
-            if(!((board[i][0].getValue() == board[i][1].getValue() == board[i][2].getValue()) & board[i][0].filled())){
-                return false;
+            //check row works
+            if ((board[i][0].getValue() == board[i][1].getValue() && board[i][1].getValue() == board[i][2].getValue() && board[i][0].filled())){
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     const checkColumn = () => {
@@ -43,64 +70,60 @@ function gameBoard() {
         */
         for(let i = 0; i < columns; i++){
             // checks values for column equal and not equal to empty acts as unless if true this statement will not run
-            if(!((board[0][i].getValue() == board[1][i].getValue() == board[2][i].getValue()) & board[i][0].filled())){
-                return false;
+            if(((board[0][i].getValue() == board[1][i].getValue() && board[1][i].getValue() == board[2][i].getValue()) && board[0][i].filled())){
+                //column works
+                //console.log(`Col that won ${i}`)
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     const checkDiagonals = () => {
+        //works 
         //two unless statements
         //top left to bottom right
-        if(!((board[0][0].getValue() == board[1][1].getValue() == board[2][2].getValue()) & board[i][0].filled())){
-            return false;
+        if(((board[0][0].getValue() == board[1][1].getValue() && board[1][1].getValue() == board[2][2].getValue()) && board[0][0].filled())){
+            return true;
         }
          //top right to bottom left
-        if(!((board[0][2].getValue() == board[1][1].getValue() == board[2][0].getValue()) & board[i][0].filled())){
-            return false;
+        if(((board[0][2].getValue() == board[1][1].getValue() && board[1][1].getValue() == board[2][0].getValue()) && board[2][0].filled())){
+            return true;
         }
-        return true;
+        return false;
     }
     // checkRow, Col and Diagonal all true
-    const checkWin = () => (checkRow & checkColumn & checkDiagonals)
-
+    const checkWin = () => (checkRow() || checkColumn() || checkDiagonals()) ? true : false
+    const debugWin = () => {
+        console.log(checkRow())
+        console.log(checkColumn())
+        console.log(checkDiagonals())
+    }
+    const checkTie = () => {
+        for(let i = 0; i < rows; i++){
+            for(let j = 0; j < columns; j++){
+                // empty tile
+                if(!(board[i][j].filled())){
+                    return false
+                }
+            }
+        }
+        return true
+    }
     const printBoard = () => {
         const boardWithValues = board.map((row) => row.map((tile) => tile.getValue()))
         console.log(boardWithValues);
+        
+        console.log("Debug:")
+        debugBoard()
+    }
+    // debug to fix win code not working
+    const debugBoard = () => {
+        const boardWithValues = board.map((row) => row.map((tile) => tile.filled()))
+        console.log(boardWithValues);
     }
     //factory game board object
-    return {checkWin, getBoard, generateBoard, printBoard}
-}
-//set tile
-function setTile(playerNum, gameboard) {
-    /*
-    Gonna be like player (1) or player (2)
-    Excepting Number
-    */
-    const board = gameboard
-    const player = playerNum
-    //added symbol not var so it is still inside of function scope
-    let symbol;
-
-    switch(player){
-        case 1:
-            symbol = 'x';
-            break;
-        case 2:
-            symbol = 'o';
-            break;
-        default:
-            console.log("Symbol not working");
-            symbol = "";
-    }
-    /*
-    private variable symbol is set
-    Closure is used so function just needs coordinates (row,col)
-    */
-    return function AddPiece(row,col){
-        board[row][col].addToken(symbol)
-    }
+    return {checkWin, getBoard, generateBoard, printBoard, setTile, checkTie, debugWin}
 }
 function Tile() {
     let value = "";
@@ -113,7 +136,7 @@ function Tile() {
     const getValue = () => value;
 
     //gives false is "" gives true anything else
-    const filled = () => (value = "") ? false: true;
+    const filled = () => (value == "") ? false : true;
 
     return {
         addToken,
@@ -123,11 +146,11 @@ function Tile() {
 }
 //closure on player
 //console only
-function Player(name, num, board) {
+function Player(name, num) {
     this.name = name
     this.num = num
     // return function AddPiece(row,col)
-    this.setTile = setTile(num, board)
+    //this.setTile = setTile(num, board)
 }
 
 
@@ -140,6 +163,23 @@ function GameController() {
         board.generateBoard()
         //prints board
         printNewRound()
+    }
+    const checkend = () => {
+        tie = board.checkTie()
+
+        if(tie){
+            console.log("Tie has occured")
+            end()
+        }
+        win = board.checkWin()
+        console.log(win)
+        if(win){
+            console.log(`${getActivePlayer().name} has won`)
+            end()
+        }
+    }
+    const end = () => {
+        board.debugWin()
     }
     
     let activePlayer = players[0]
@@ -159,13 +199,16 @@ function GameController() {
     const consoleInput = () => {
         const row = prompt("Row")
         const col = prompt("Col")
-        activePlayer.setTile(row, col)
+        //setTile = (playerNum, row, col) 
+        board.setTile(getActivePlayer().num, row, col)
+        // removed changed to add settile to board not player activePlayer.setTile(row, col)
     }
     const playRound = () => {
         //console input
         consoleInput()
         //prints board
         printNewRound()
+        checkend()
         switchPlayerTurn()
     }
     return {
